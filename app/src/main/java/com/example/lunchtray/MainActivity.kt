87647e08f4dbb2bd49dbf2a0fb60afe3f01a9e30
josorian <josejaviersoriano54@gmail.com>
2024-02-27 01:47:31 +0100

    Initial commit

---
+++ app/src/main/java/com/example/lunchtray/MainActivity.kt
@@ -0,0 +1,21 @@
+
+package com.example.lunchtray
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.lunchtray.ui.theme.LunchTrayTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LunchTrayTheme {
+                LunchTrayApp()
+            }
+        }
+    }
+}
+
+
